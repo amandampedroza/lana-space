d4a8d08759d31a8633f6985f3d83ed7621daf60e
amandampedroza <amanda.pedroza@sptech.school>
2024-11-20 15:19:44 -0300

    css

--- src/database/script.sql
+++ src/database/script.sql
@@ -1,23 +1,24 @@
 create database LanaSpace;
-
+drop database LanaSpace;
 use LanaSpace;
 
 create table usuario (
 idUsuario int primary key auto_increment,
 nome varchar(45),
 email varchar(255),
-senha varchar(40)
+senha varchar(40),
+dataNascimento date
 );
 
 create table respostas (
+idResposta int primary key auto_increment,
 albumFavorito varchar(45),
-musicaFavorita varchar(45),
-dtNascimento date,
 fkUsuario int,
 constraint fkUsuarioRespostas foreign key (fkUsuario) references usuario(idUsuario)
 );
 
 
+select * from respostas;
 
-
-select * from usuario;
+select * from usuario
+where nome like '%m____';
